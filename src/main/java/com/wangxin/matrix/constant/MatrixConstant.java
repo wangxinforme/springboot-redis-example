899f75e28c5a032e1a927cc70e48e4d81b14f2d8
xwang <xwang@wangxin-PC.handpay.com.cn>
2017-12-20 17:25:41 +0800

    更新

--- src/main/java/com/wangxin/matrix/constant/MatrixConstant.java
+++
@@ -1,8 +0,0 @@
-package com.wangxin.matrix.constant;
-
-public class MatrixConstant {
-    public static final String CGLIB = "Cglib";
-
-    public static final String PROXY_TYPE_REFLECTIVE = "Reflective Aop Proxy";
-    public static final String PROXY_TYPE_CGLIB = "Cglib Aop Proxy";
-}
